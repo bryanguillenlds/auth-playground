682b357995b12e7865e246325d15d0e68a6196aa
Bryan Guillén <bryanguillenlds@gmail.com>
2025-11-18 20:44:20 -0400

    AuthService and RegisterUser controller method.
    - Implemented AuthService for user registration logic, including email existence check and user creation.
    - Updated AuthController to handle registration errors using CustomError.
    - Refactored constructor in controller to use dependency injection for AuthService.
    - Added error logging in internalServerError method of CustomError.
    - Updaated routes to utilize the updated AuthController with AuthService.

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,18 +1,41 @@
 import { Request, Response } from "express";
+import { RegisterUserDto } from "../../domain/dtos/auth/register-user.dto";
+import { AuthService } from "../services/auth.service";
+import { CustomError } from "../../domain/errors/custom.error";
 
 export class AuthController {
-  // DEPENDENCY INJECTION
-  constructor() {}
+  // DEPENDENCY INJECTION of the AuthService
+  // This pattern is necessary to avoid coupling between the controller and the service
+  constructor(public readonly authService: AuthService) {}
 
-  registerUser = (req: Request, res: Response) => {
-    res.json("registerUser");
+  private handleError = (res: Response, error: unknown) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+
+    console.log(`${error}`);
+    return res.status(500).json({ error: "Internal server error" });
   };
 
-  loginUser = (req: Request, res: Response) => {
+  registerUser = async (req: Request, res: Response) => {
+    const [errorMessage, registerUserDto] = RegisterUserDto.create(req.body);
+
+    if (errorMessage) return res.status(400).json({ error: errorMessage });
+
+    try {
+      const user = await this.authService.registerUser(registerUserDto!);
+
+      res.json(user);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  };
+
+  loginUser = async (req: Request, res: Response) => {
     res.json("loginUser");
   };
 
-  validateEmail = (req: Request, res: Response) => {
+  validateEmail = async (req: Request, res: Response) => {
     res.json("validateEmail");
   };
 }
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthService } from "../services/auth.service";
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
+    const authService = new AuthService();
 
-    const { registerUser, loginUser, validateEmail } = new AuthController();
+    const { registerUser, loginUser, validateEmail } = new AuthController(
+      authService
+    );
 
     router.post("/login", loginUser);
     router.post("/register", registerUser);
